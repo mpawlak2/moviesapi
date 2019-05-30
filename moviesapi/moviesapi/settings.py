37072609917b70d72a5f30e3e89aa656cf10977b
Mateusz Pawlak <mpawlak62@gmail.com>
2019-05-30 16:50:43 +0200

    Load the OMDB API key from environmental vars

--- moviesapi/moviesapi/settings.py
+++ moviesapi/moviesapi/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
--- moviesapi/moviesapi/wsgi.py
+++ moviesapi/moviesapi/wsgi.py
@@ -10,6 +10,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviesapi.settings')
 
