ff6f4fb15f4edb4692182539113943db23b32e1f
Mateusz Pawlak <mpawlak62@gmail.com>
2019-05-31 06:56:57 +0200

    Add year field on the Movie model

--- moviesapi/movies/models.py
+++ moviesapi/movies/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=300)
+    year = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return " ".join([self.title, self.year])
