8d2d99a088feddad7ba9d4750cf70d1110b80dd8
Mateusz Pawlak <mpawlak62@gmail.com>
2019-05-31 17:22:09 +0200

    Change urls so they don't end with a slash

--- moviesapi/movies/urls.py
+++ moviesapi/movies/urls.py
@@ -5,7 +5,7 @@ from movies.views import CommentsAPI, MoviesAPI, TopAPI
 app_name = "movies"
 
 urlpatterns = [
-    path("movies/", MoviesAPI.as_view(), name="movies"),
-    path("comments/", CommentsAPI.as_view(), name="comments"),
-    path("top/", TopAPI.as_view(), name="top"),
+    path("movies", MoviesAPI.as_view(), name="movies"),
+    path("comments", CommentsAPI.as_view(), name="comments"),
+    path("top", TopAPI.as_view(), name="top"),
 ]
